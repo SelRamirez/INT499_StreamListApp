e520f545deb236c8fa0345f804e8f5f6b3d66d9f
SelRamirez <selene.ramirez@student.uagc.edu>
2026-01-21 00:50:25 -0600

    Add files via upload

--- src/Links.js
+++ src/Links.js
@@ -6,7 +6,7 @@ function Navbar() {
         
         <nav className="nav">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=video_camera_front" />
-            
+        
             <Link to="/" className="site-name">EZ StreamList <span class="material-symbols-outlined">video_camera_front</span></Link>
 
             <div className="nav-page">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,58 @@
 import React, {useState} from 'react';
+import EditTitle from './EditTitle';
+let newId =0;
+
+
 
 function Home () {
-    
+        
+    const [movieList, setMovies] = useState([]);
     const [value,setValue] = useState('');
-
+    const [editValue,setEditValue] = useState('');
+    
     const handleChange = (e) => {
         setValue(e.target.value);
     }
-    
+
+    const handleEditChange = (e) => {
+        setEditValue(e.target.value);
+    }
     
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("You searched for " + value + ".");
+        if (value.trim() != '') {
+
+            setMovies([...movieList,{id: newId++, movieTitle: value, editMode: false}]);
+            console.log("You added " + value + ".");
+            setValue('');
+            console.log(movieList);
+            }
+        else {
+            console.log('No movie added.')
+        }
+    }
+
+    const editMovieTitle = (e, movieId) => {
+        e.preventDefault();
+        console.log('editMovieTitle called '+ editValue);
+        changeMovieTitle(movieId);
+
+    }
+
+    const changeMovieTitle= (movieId) => {setMovies(movieList.map((movie) => movie.id === movieId ? {...movie, movieTitle: editValue} : movie));}
+    const changeEditMode = (movieId) => {setMovies(movieList.map((movie) => movie.id === movieId ? {...movie, editMode: !movie.editMode } : movie)); }
+    const handleEditButton = (movieId) => {
+        console.log('Button worked? Movie ID: '+movieId);
+        changeEditMode(movieId);
     };
-    
+
     return (
+
         <div className="home">
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+           
             <h1>Welcome to the EZ StreamList Homepage!</h1>
-            <h2>Try searching a movie title.</h2>
+            <h2>Try adding a movie title.</h2>
             <form onSubmit={handleSubmit}>
                 <label>Title: </label>
                 <input
@@ -26,8 +61,36 @@ function Home () {
                     onChange={handleChange}
                 />
                 
-                <button type = "submit">Search</button>
+                <button type = "submit">Add to List</button>
             </form>
+            <h2>Your Movie List</h2>
+            <ul id='movieList' className='movie-list'>
+                    {movieList.map(movie => (
+
+                        //Makes new item in list 
+                        <li key={movie.id}>
+                            
+                            <button onClick={()=> {
+                                setMovies(
+                                    movieList.filter(m => m.id !== movie.id
+                                    )
+                                );
+                            }}>
+                                <span class="material-icons">
+                                delete
+                                </span>
+                            </button>
+                            <button class="material-icons" onClick={()=>handleEditButton(movie.id)}> 
+                                edit
+                            </button>
+                            
+                            <div className='movie-title'>{movie.movieTitle}</div>
+                            {movie.editMode ? (<div> <input type="text" value={editValue} onChange={handleEditChange} />
+                                <button onClick={editMovieTitle}>Update</button></div>): (null)}
+                            </li>
+                    ))}
+                </ul>
+                
         </div>
     )
 }
