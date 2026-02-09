efd42e9630f94bbb9cbf4cd620cd0af80048690f
SelRamirez <selene.ramirez@student.uagc.edu>
2026-02-09 09:05:20 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,3 +15,7 @@ root.render(
   </React.StrictMode>
 );
 
+
+serviceWorkerRegistration.register();
+
+reportWebVitals();
